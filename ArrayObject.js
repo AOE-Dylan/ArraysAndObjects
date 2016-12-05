847bdfa3db6905deec2850a585956bc22c75f800
AOE-Dylan <dylantt2000@gmail.com>
2016-12-05 08:39:39 -1000

    I has maed sum objex and arraeezz

---
+++ ArrayObject.js
@@ -0,0 +1,26 @@
+/*
+
+Dylan Tucker
+Arrays and Objects
+Dec. 5, 2016
+
+*/
+
+//vars
+//ARRAYS
+
+var numbers = [1,2,3,4,5,6,7,8,9,1234567890];
+var names = ["Senor TAN", "Zakattack", "Yolo Baggins", "Ohai", "Basic Jeff"];
+var colors = ["black", "ron burgandy", "highlighter"];
+var adj = ["smart", "special case", "yummy", "tiny whiny", "ferocious"];
+var slang = ["Scrap", "beef", "dakine", "li dat", "pau hana"];
+
+//OBJEX Trolool
+
+var donut = {
+  toppings : ["Sprinkles", "Chocolate frosting", "o'hare air"],
+  fillings : "jelly",
+  shape : "square",
+  count : 12
+}
+
